44a477c4b39a9001c99386089e2eea040d20ee64
YuriiLeo <yuriileontenko@gmail.com>
2022-07-17 13:09:28 +0300

    додав розмітку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,73 @@
+// 1. Відмалювати розмітку (reduce, деструктур + шаблонний рядок)
+// 2. Підставити в розмітку поля (${})
+// 3. Навісити клік на галерею  (делегування)
+// 4. Визначення ел по якому був клік (event.target)
+// 5. Підставити данні у модалку з дата сорс (dataSet)
+// 6. 
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// 1 створення розмітки
 console.log(galleryItems);
+
+function createImgMarkup (array) {
+    return array.reduce(
+        (acc, {original, preview, description}) => acc +
+          `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+              />
+            </a>
+          </div>`,
+        ""
+    );
+}
+
+const ImgMarkup = createImgMarkup(galleryItems);
+// console.log(ImgMarkup);
+
+const galleryImgContainer = document.querySelector(".gallery");
+galleryImgContainer.insertAdjacentHTML('beforeend', ImgMarkup);
+
+// 2 делегування
+galleryImgContainer.addEventListener('click', onGalleryImgContainerClick);
+
+function onGalleryImgContainerClick(evt) {
+    const isGalleryImageEl = evt.target.classList.contains("gallery__image")
+    if (!isGalleryImageEl) {
+        return;
+    }
+    evt.preventDefault();
+    console.log(evt.target.dataset.source);   
+
+    const imgEl = evt.target;
+    const parentGalleryImage = imgEl.closest('.gallery__item')
+
+    removeOpenModalClass();
+    addOpenModalClass(parentGalleryImage)
+
+    console.log(parentGalleryImage);
+
+    // модалка = imgEl.dataset.source;   
+    
+    // console.log(curentOpenImage);
+    // console.log(imgEl.classList);
+}
+
+function removeOpenModalClass() {
+    const curentOpenImage = document.querySelector('.gallery__item.modal__open');
+
+    if (curentOpenImage) {
+        curentOpenImage.classList.remove('modal__open')
+    }
+}
+
+function addOpenModalClass(img) {
+    img.classList.add('modal__open');
+}
+
+
+// const curentActiveImg = document.querySelector('.modal-open');
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
+// 1. Відмалювати розмітку (map + шаблонний рядок  )
+// 2. Підставити в розмітку поля (${})
+// 3. Навісити клік на галерею  (делегування)
+// 4. Визначення ел по якому був клік (event.target)
+// 5. Підставити данні у модалку з дата сорс (dataSet)
+
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+function createImgMarkup (array) {
+    return array
+        .map(
+            (item) => 
+                `<a class="gallery__item" href="${item.original}">
+                   <img 
+                   class="gallery__image" 
+                   src="${item.preview}" 
+                   alt="${item.description}" />
+                </a>`
+    )
+        .join("");
+}
+
+const result = createImgMarkup(galleryItems);
+console.log(result);
+
+const galleryImg = document.querySelector(".gallery");
+galleryImg.insertAdjacentHTML('beforeend', result);
+
